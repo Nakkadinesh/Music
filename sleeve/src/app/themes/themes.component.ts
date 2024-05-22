ea792b721e24d6f4f8a3c061a8c1a1f7cbe551cb
dinesh yadav <dineshyadav70326@gmail.com>
2024-05-22 16:43:24 +0530

    Modified

--- sleeve/src/app/themes/themes.component.ts
+++ sleeve/src/app/themes/themes.component.ts
@@ -9,7 +9,7 @@ export class ThemesComponent {
 services:any[];
 constructor(){
   this.services=[
-{bgi:"assets/images/playlist1.jpg",para:"tSwitch between themes with just a click.Modify the built-in themes or create your own using Sleeve’s extensive preferences.",img:"assets/images/Artwork1.jpg",head:"Change it up"},
+{bgi:"assets/images/playlist1.jpg",para:"Switch between themes with just a click.Modify the built-in themes or create your own using Sleeve’s extensive preferences.",img:"assets/images/Artwork1.jpg",head:"Change it up"},
 {bgi:"assets/images/playlist3.jpg",para:"Export your themes and share them with friends using the handy new Sleeve Theme file format.Install themes from anywhere with a double-click or a drag and drop.",img:"assets/images/AppDoppler.png",head:"Shareable"}
   ]
 }
